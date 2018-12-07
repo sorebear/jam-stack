2e6b3c1aaf09904e6c3bc0a1d976c15bd2b3126b
Soren Baird <sorenbaird@gmail.com>
2018-12-06 19:08:29 -0800

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-diamond"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Soren Baird - Web Developer</h1>
+                <p>I live and JAM in Orange County</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
